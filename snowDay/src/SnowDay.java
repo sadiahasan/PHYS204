b2d7f6163ea6a200ce9ecc2d212cd77e280a3f76
Sadia Hasan <sadiazh729@gmail.com>
2017-02-27 20:43:08 -0500

    Update SnowDay.java

--- snowDay/src/SnowDay.java
+++ snowDay/src/SnowDay.java
@@ -7,24 +7,32 @@ import java.util.Scanner;
     public static void main(String[] args) throws FileNotFoundException{
         
        
-        File file = new File("morecities.txt");
+        File file = new File("random.txt");
         Integer[] array=new Integer[10];
         Arrays.fill(array,0);
      
         Scanner scanner = new Scanner(file);
-        
+        int total = 0;
         while(scanner.hasNext()){
         int x = scanner.nextInt();
         String s = Integer.toString(x);
         char t = s.charAt(0);
         int p = Character.getNumericValue(t);
         array[p]++;
+        total++;
          
         }
-        System.out.println("HERE WE ARE");
-        for(int i=0; i<=10; i++){
+      
+        for(int i=0; i<=9; i++){
             System.out.println(i+" : "+array[i]);
+        
+            probability(array[i],total);
         }
+    }
+
+    private static void probability(Integer integer, int total) {
+        double i = ((double)integer/(double)total);
+        System.out.println(i*100);
     }   
     }  
 
